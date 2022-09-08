b4054e075cb1429c476547359cccf7b042a8ff52
WilsonValer <nederig21@gmail.com>
2022-09-08 10:51:41 -0500

    task08

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+def uppercase(str):
+    for i in str:
+        j = ord(i)
+        if j >= 97 and j <= 122:
+            j = j -32
+        print(chr(j), end='')
+    print()
