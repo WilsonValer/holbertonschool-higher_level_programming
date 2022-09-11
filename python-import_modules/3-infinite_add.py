bcdebea9a814fea0b21e080bba87efe1f34335bd
WilsonValer <nederig21@gmail.com>
2022-09-11 15:53:08 -0500

    task03

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ == '__main__':
+    import sys
+    num_argu = len(sys.argv)
+    suma = 0
+    for i in range(1, num_argu):
+        suma += int(sys.argv[i])
+    print(suma)
+
+
