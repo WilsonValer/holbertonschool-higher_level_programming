1fabaaf6e42958c6b7a5c896cb397410ff5af29d
WilsonValer <nederig21@gmail.com>
2022-09-11 00:45:59 -0500

    task02 eidted

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -10,5 +10,5 @@ if __name__ == '__main__':
         print("1 argument:")
     else:
         print("{} arguments:".format(len(sys.argv) - 1))
-        for i in range(1, num):
-            print("{}: {}".format(i, sys.argv[i]))
+    for i in range(1, num):
+        print("{}: {}".format(i, sys.argv[i]))
