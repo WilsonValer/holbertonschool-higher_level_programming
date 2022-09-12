cd848856abee48a384ef8db3aaedc972b94e52c6
WilsonValer <nederig21@gmail.com>
2022-09-12 18:42:33 -0500

    tasks

---
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    size = len(my_list)
+    for i in range(int(size / 2)):
+        num = my_list[i]
+        my_list[i] = my_list[size - i - 1]
+        my_list[size - i - 1] = num
+    for i in my_list:
+        print(f"{i}")
---
+++ python-data_structures/prueba.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+prueba = __import__('0-print_list_integer').print_list_integer
+my_list = [1, 2, 3, 4, 5, 6]
+prueba(my_list)
