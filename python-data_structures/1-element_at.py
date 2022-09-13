f4d78f2c56c00e9b4ce2bb4613758e2442808600
WilsonValer <nederig21@gmail.com>
2022-09-13 18:50:21 -0500

    task01

--- python-data_structures/1-element_at.py
+++ python-data_structures/1-element_at.py
@@ -3,3 +3,7 @@ def element_at(my_list, idx):
     for i in my_list:
         if i == idx:
             return my_list[idx]
+        elif idx < 0:
+            return None
+        elif idx > len(my_list):
+            return None
