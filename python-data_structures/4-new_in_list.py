e60b26f6c03862b22bb7478f85b35fe0d729cc7f
WilsonValer <nederig21@gmail.com>
2022-09-13 20:12:47 -0500

    task04

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    for i in my_list:
+    for i in range(len(my_list)):
         if i == idx:
             other_list = my_list[:]
             other_list[idx] = element
