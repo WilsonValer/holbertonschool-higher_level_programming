d0fc23a8c04a1c7f6b536315116d947600efd98a
WilsonValer <nederig21@gmail.com>
2022-09-13 21:38:35 -0500

    task11

---
+++ python-data_structures/11-delete_at.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    if not my_list:
+        return
+    large = len(my_list)
+    for i in range(large):
+        if i == idx:
+            del my_list[idx]
+            return my_list
+        elif idx < 0:
+            return my_list
+        elif idx >= large:
+            return my_list
