994304a336cabf4c8b533eec9347c1b146e2c5a7
WilsonValer <nederig21@gmail.com>
2022-09-13 20:14:18 -0500

    task04

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -7,5 +7,5 @@ def new_in_list(my_list, idx, element):
             return other_list
         elif idx < 0:
             return my_list
-        elif idx > len(my_list):
+        elif idx >= len(my_list):
             return my_list
