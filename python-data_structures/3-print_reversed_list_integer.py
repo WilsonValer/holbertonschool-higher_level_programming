c7b86c58ce027c98eeefc78872e60703d2995757
WilsonValer <nederig21@gmail.com>
2022-09-13 19:48:24 -0500

    task03  edited

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
+    if not my_list:
+        return
     size = len(my_list)
     for i in range(int(size / 2)):
         num = my_list[i]
