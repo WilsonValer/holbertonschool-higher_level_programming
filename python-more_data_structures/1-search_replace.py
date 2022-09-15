5ab025d69ed7d55b6478932d1a3a544f02949142
WilsonValer <nederig21@gmail.com>
2022-09-15 17:52:57 -0500

    task01

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -4,5 +4,5 @@ def search_replace(my_list, search, replace):
     length = len(my_list)
     for elem in range(length):
         if my_list[elem] == search:
-            my_list[elem] = 89
+            new_list[elem] = 89
     return new_list
