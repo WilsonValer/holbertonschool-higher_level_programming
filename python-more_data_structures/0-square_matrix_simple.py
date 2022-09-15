423ac01c3f2d2bc995469b6598eb77f0cd0a1a58
WilsonValer <nederig21@gmail.com>
2022-09-15 17:04:23 -0500

    task00 was edited

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -3,8 +3,8 @@ def square_matrix_simple(matrix=[]):
     large = len(matrix)
     new_list = matrix[:]
     for row in range(large):
-        large_2 = len(matrix[0])
+        large_2 = len(matrix[row])
         new_list[row] = matrix[row][:]
         for elem in range(large_2):
-            new_list[elem] = matrix[row][elem] ** 2
+            new_list[row][elem] = matrix[row][elem] ** 2
     return new_list
