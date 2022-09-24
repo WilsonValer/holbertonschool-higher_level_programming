446068c7674626d074b266ede0b435c06fa93933
WilsonValer <nederig21@gmail.com>
2022-09-24 12:44:34 -0500

    task02

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -5,13 +5,15 @@ Prototype matrix_divided(matrix, div).
 function that divides all elements of a matrix.
 """
 
+
 def matrix_divided(matrix, div=1):
     """ Doc """
     if type(div) is not int and type(div) is not float:
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    if type(matrix) is not list or (len(matrix) == 0) or type(matrix[0]) is not list or (len(matrix[0]) == 0):
+    if type(matrix) is not list or (len(matrix) == 0) or\
+            type(matrix[0]) is not list or (len(matrix[0]) == 0):
         raise TypeError("matrix must be a matrix (list of lists) of \
 integers/floats")
     for row in matrix:
