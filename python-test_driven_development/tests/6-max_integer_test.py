08615a7b7c75a1e22368aed7a0c39a0486b2aceb
WilsonValer <nederig21@gmail.com>
2022-09-24 18:38:16 -0500

    task06

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -8,14 +8,23 @@ max_integer = __import__('6-max_integer').max_integer
 class TestMaxInteger(unittest.TestCase):
     """Set of test to validate the functionality of the function max integer"""
 
-    def test_doc(self):
-        """Validating the documentation in the file"""
-        self.assertTrue(len(max_integer.__doc__) > 0)
 
     def test_max(self):
         """Test normal cases without error"""
         result = max_integer([2, 7, 6])
-        assertEqual(result, 6)
+        self.assertEqual(result, 7)
+
+    def negative(self):
+        result = max_integer([-5, -7, -1], -1)
+        self.assertEqual(result, -1)
+
+    def string():
+        with self.assertRaises(TypeError):
+            max_integer([-5, "wilson"])
+
+    def error():
+        result = max_integer([7, 6, "string"])
+        self.assertIsInstance(result, int, "validate type")
 
 
 
