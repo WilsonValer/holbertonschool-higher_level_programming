041282b580169fb507bf3b752e71cfc63cda1e45
WilsonValer <nederig21@gmail.com>
2022-09-24 12:31:48 -0500

    task02

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -5,42 +5,24 @@ Prototype matrix_divided(matrix, div).
 function that divides all elements of a matrix.
 """
 
-
-def matrix_divided(matrix, div):
-    """
-    Function that divides all elements of a matrix.
-    Args:
-        matrix (int): list of element to divide
-        div (int): divider
-    Returns:
-        list: the new list with all elements divided.
-    """
-    leng = 0
+def matrix_divided(matrix, div=1):
+    """ Doc """
+    if type(div) is not int and type(div) is not float:
+        raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
+    if type(matrix) is not list or (len(matrix) == 0) or type(matrix[0]) is not list or (len(matrix[0]) == 0):
+        raise TypeError("matrix must be a matrix (list of lists) of \
+integers/floats")
+    for row in matrix:
+        if type(row) is not list:
+            raise TypeError("matrix must be a matrix (list of lists) of \
+integers/floats")
+        if len(row) != len(matrix[0]):
+            raise TypeError("Each row of the matrix must have the same size")
+        for c in row:
+            if type(c) is not int and type(c) is not float:
+                raise TypeError("matrix must be a matrix (list of lists) of \
+integers/floats")
 
-    if not isinstance(div, (int, float)):
-        raise TypeError("div must be a number")
-
-    msg_1 = "matrix must be a matrix (list of lists) of integers/floats"
-
-    if not matrix or not isinstance(matrix, list):
-        raise TypeError(msg_1)
-
-    msg_2 = "Each row of the matrix must have the same size"
-
-    for elements in matrix:
-        if not elements or not isinstance(elements, list):
-            raise TypeError(msg_1)
-
-    if leng != 0 and len(elements) != leng:
-        raise TypeError(msg_2)
-
-    for num in elements:
-        if not isinstance(num, (int, float)):
-            raise TypeError(msg_1)
-
-    leng = len(elements)
-
-    mtx = [[round((fil[i]/div), 2) for i in range(len(fil))] for fil in matrix]
-    return mtx
+    return [[round(item / div, 2) for item in row] for row in matrix]
