9ea0f474f1f00a03372b10ec63837c585b826cfe
WilsonValer <nederig21@gmail.com>
2022-09-24 19:07:27 -0500

    task06

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -15,18 +15,30 @@ class TestMaxInteger(unittest.TestCase):
         self.assertEqual(result, 7)
 
     def negative(self):
-        result = max_integer([-5, -7, -1], -1)
+        result = max_integer([-5, -7, -1])
         self.assertEqual(result, -1)
 
     def string():
         with self.assertRaises(TypeError):
             max_integer([-5, "wilson"])
 
-    def error():
-        result = max_integer([7, 6, "string"])
-        self.assertIsInstance(result, int, "validate type")
+    def nega_positive():
+        result = max_integer([4, 5, -7, 3])
+        self.assertEqual(result, 5)
+    
+    def empty():
+        with self.assertRaises(TypeError):
+            max_integer([])
 
+    def tes_error():
+        result = max_integer([8, 2, "rt"])
+        self.assertEqual(result,10, "no is result" )
+        self.assertIsInstance(result, int, " validate type")
 
+    def test_None(self):
+        self.assertIsNone(max_integer([7, 9, 15]), None,)
+        self.assertIsNone(max_integer(), None)
+        self.assertIsNone(max_integer([None]), None)
 
 
 if __name__ == '__main__':
