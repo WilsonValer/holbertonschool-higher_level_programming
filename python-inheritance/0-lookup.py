057e812e55bc85c7a2d2df158abe07ba4a4d5296
WilsonValer <nederig21@gmail.com>
2022-09-27 18:46:43 -0500

    task

---
+++ python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Empty class that defines a Rectangle
+"""
+def lookup(obj):
+    """ 
+    function that returns the list
+    of available attributes
+    and methods of an object:
+    """
+    return dir(obj)
