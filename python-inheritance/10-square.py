1ea9814714f468f03c4ee4f941950060d16cbb61
WilsonValer <nederig21@gmail.com>
2022-09-29 16:05:10 -0500

    task10

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""Class rectangle inherent"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """ Class that defines a rectangle from BaseGeometry Class """
+
+    def __init__(self, width, height):
+        """ Initializes instance """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return "[Rectangle] {:d}/{:d}".format(self.__width, self.__height)
+
+
+class Square(Rectangle):
+    """ clas Square """
+    def __init__(self, size):
+        super().__init__(size, size)
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
