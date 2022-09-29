2953f53cf9c94aac2858deaa15fe57ff2bbb3676
WilsonValer <nederig21@gmail.com>
2022-09-29 16:50:34 -0500

    task

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Class rectangle inherent"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ clas Square """
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
+
+
+    def __str__(self):
+        return "[Square] {}/{}".format(self.__size, self.__size)
+
