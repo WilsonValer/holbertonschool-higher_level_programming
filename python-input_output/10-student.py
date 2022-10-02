b2b7ec3d2cd43a5ce01b4697fce0b43ade1d36fd
WilsonValer <nederig21@gmail.com>
2022-10-02 14:42:25 -0500

    task10

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -3,7 +3,7 @@
 
 
 class Student():
-    """ my class """
+    """ all my class """
     def __init__(self, first_name, last_name, age):
         self.first_name = first_name
         self.last_name = last_name
@@ -11,16 +11,17 @@ class Student():
 
     def to_json(self, attrs=None):
         """ public method """
+        empty_dic = {}
+
         dicty = self.__dict__
 
-        if attrs is  None:
+        if attrs is None:
             return dicty
 
         if attrs:
-            empty_dic = {}
             for elem in range(len(attrs)):
                 if attrs[elem] in dicty:
                     empty_dic[attrs[elem]] = dicty[attrs[elem]]
             return empty_dic
         else:
-            return dicty
+            return empty_dic
