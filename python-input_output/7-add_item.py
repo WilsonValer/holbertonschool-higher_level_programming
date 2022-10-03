53127bb6ecd68f88879103bced6bad7c980beec5
WilsonValer <nederig21@gmail.com>
2022-10-02 19:46:32 -0500

    task07

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
+import sys
+import json
 """This program take the file add_item.json, and add the
 parameters to the list inside this file.
 - If the file doesn't exist create it.
-- If no exist parameters do nothing or create the list if the file is empty.
+- If no exist parameters do nothing or
+create the list if the file is empty.
 """
-
-
-import sys
-import json
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+
 
 lenght = len(sys.argv)
 my_list = []
@@ -17,6 +17,7 @@ address = "add_item.json"
 
 for i in range(1, lenght):
     my_list.append(sys.argv[i])
+
 
 new_list = load_from_json_file(address)
 
