0623c6595f3aa93154f1855e1a12519fa23d22d1
WilsonValer <nederig21@gmail.com>
2022-10-02 19:52:23 -0500

    task07

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
+''' Adds all arguments to a Python list '''
+
+
 import sys
 import os.path
-"""This program take the file add_item.json, and add the
-parameters to the list inside this file.
-- If the file doesn't exist create it.
-- If no exist parameters do nothing or
-create the list if the file is empty.
-"""
+
+
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
