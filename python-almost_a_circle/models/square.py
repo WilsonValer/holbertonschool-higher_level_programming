44aa87d4505284efd6a893134868607626c77517
WilsonValer <nederig21@gmail.com>
2022-10-06 17:40:00 -0500

    Update the class Square by adding

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -8,6 +8,17 @@ class Square(Rectangle):
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """property for getter"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """property for setter"""
+        self.width = value
+        self.height = value
+
     def __str__(self):
         """Write the class Square that inherits from Rectangle"""
         return ("[Square] ({:d}) {:d}/{:d} - {:d}"
