a5e7ce9162e19ddaa98af99ab62b8dda2ab78de5
WilsonValer <nederig21@gmail.com>
2022-10-08 14:11:35 -0500

    test for base

--- python-almost_a_circle/tests/test_models/test_base.py
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 """ base test """
 
+import os
+from models.square import Square
+from io import StringIO
 import unittest
 from models.base import Base
 from models.rectangle import Rectangle
@@ -40,6 +43,56 @@ class TestBase(unittest.TestCase):
         self.assertEqual(string_01.id, "unicornio")
         self.assertEqual(string_02.id, "sagitario")
 
+    def test_to_json_string_AND_from_json_string(self):
+        """from json string"""
+        list_input = [
+            {'id': 89, 'width': 10, 'height': 4},
+            {'id': 7, 'width': 1, 'height': 7}
+        ]
+        json_list_input = Rectangle.to_json_string(list_input)
+        list_output = Rectangle.from_json_string(json_list_input)
+        self.assertIsInstance(list_input, list)
+        self.assertIsInstance(json_list_input, str)
+        self.assertIsInstance(list_output, list)
 
+    @classmethod
+    def setUpClass(cls):
+        Base._Base__nb_objects == 0
+        cls.r1 = Rectangle(3, 5, 1, id=9)
+        cls.r3 = Rectangle(2, 4, id=11)
+        cls.s1 = Square(5, id=99)
+        cls.s2 = Square(7, 9, 1, id=78)
 
+    def test_save_to_file_AND_load_from_file(self):
+        list_rectangles_input = [self.r1, self.r3]
 
+        Rectangle.save_to_file(list_rectangles_input)
+        self.assertTrue(os.path.isfile('Rectangle.json'))
+        with open('Rectangle.json', 'r') as f:
+            r_total = sum(1 for _ in f)
+        self.assertGreater(r_total, 0)
+        list_rectangles_output = Rectangle.load_from_file()
+
+        for rect in list_rectangles_input:
+            self.assertIsInstance(rect, Rectangle)
+
+        for rect in list_rectangles_output:
+            self.assertIsInstance(rect, Rectangle)
+
+        list_squares_input = [self.s1, self.s2]
+
+        Square.save_to_file(list_squares_input)
+        self.assertTrue(os.path.isfile('Square.json'))
+
+        with open('Square.json', 'r') as f:
+            s_total = sum(1 for _ in f)
+        self.assertGreater(s_total, 0)
+        list_squares_output = Square.load_from_file()
+
+        for square in list_squares_input:
+            self.assertIsInstance(square, Square)
+
+        for square in list_squares_output:
+            self.assertIsInstance(square, Square)
+
+        Base._Base__nb_objects -= 4
