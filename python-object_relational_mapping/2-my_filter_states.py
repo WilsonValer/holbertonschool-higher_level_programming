939fc888a2de4b56471e4fa1989f1e3c06807c67
WilsonValer <nederig21@gmail.com>
2022-11-10 12:23:53 -0500

    Write a script that takes in an argument and displays all values in the states

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -10,13 +10,14 @@ if __name__ == "__main__":
     import MySQLdb
 
     db = MySQLdb.connect(host="localhost",  port=3306,
-                           user=sys.argv[1], password=sys.argv[2],
-                           database=sys.argv[3])
+                         user=sys.argv[1], password=sys.argv[2],
+                         database=sys.argv[3])
 
     cursor = db.cursor()
     state_name_searched = sys.argv[4]
 
-    will = "SELECT * FROM states WHERE name='{}' ORDER BY id ASC".format(state_name_searched)
+    will = "SELECT * FROM states WHERE name='{}'ORDER BY id ASC".format
+    (state_name_searched)
     cursor.execute(will)
     rows = cursor.fetchall()
     for row in rows:
