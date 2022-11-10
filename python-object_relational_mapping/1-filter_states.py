88f9b54bbe499a558eac6c3b51aa84e602646c7e
WilsonValer <nederig21@gmail.com>
2022-11-09 23:44:00 -0500

    task02 was edited

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -8,8 +8,8 @@ if __name__ == "__main__":
     import MySQLdb
 
     db = MySQLdb.connect(host="localhost",  port=3306,
-                           user=sys.argv[1], password=sys.argv[2],
-                           database=sys.argv[3])
+                         user=sys.argv[1], password=sys.argv[2],
+                         database=sys.argv[3])
 
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
