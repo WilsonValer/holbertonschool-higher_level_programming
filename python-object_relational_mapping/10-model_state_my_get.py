aea2159ca26d942e7e7fe898c369d3bdf6c0b855
WilsonValer <nederig21@gmail.com>
2022-11-12 14:45:16 -0500

    10. Consigue un estado

---
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""cript that lists all State objects that contain the letter a from the
+database hbtn_0e_6_usa
+"""
+if __name__ == "__main__":
+
+    import sys
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy import Column, Integer, String
+    from sqlalchemy.ext.declarative import declarative_base
+    from sqlalchemy.orm import sessionmaker
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    state_id = sys.argv[4]
+    Session = sessionmaker(engine)
+    session = Session()
+    Base.metadata.create_all(engine)
+
+    datas = session.query(State).filter(State.name == state_id)
+
+    if datas:
+        for elem in datas:
+            print("{}".format(elem.id))
+    else:
+        print("Not found")
+    session.close()
