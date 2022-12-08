894059c1e0118eb0e4660a6b7184888846479584
WilsonValer <nederig21@gmail.com>
2022-12-08 11:27:45 -0500

    3. Value of my argument

--- javascript-warm_up/3-value_argument.js
+++ javascript-warm_up/3-value_argument.js
@@ -3,12 +3,6 @@
 Write a script that prints 3 lines:”:
 */
 
-// const myArgv = process.argv.slice(2);
-/*
-for(var i = 2; i < process.argv.length; i++) {
-  console.log(process.argv[i]);
-}
-*/
 const arg = process.argv[2];
 if (arg) {
   console.log(arg);
