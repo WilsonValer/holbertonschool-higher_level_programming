31b10cf5e6a8c84113f583362f594b90eeea9837
WilsonValer <nederig21@gmail.com>
2022-12-09 23:29:41 -0500

    Write a script that prints x times “C is fun”

---
+++ javascript-warm_up/7-multi_c.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+/*
+ * Write a script that prints two arguments passed to it, in the following format: “ is ”
+*/
+
+const large = process.argv[2];
+for (let i = 0; i <= large; i++) {
+  console.log('C is fun');
+}
