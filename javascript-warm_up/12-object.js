6f6128572b8c72010297ec447f9b2275e2a3ac8c
WilsonValer <nederig21@gmail.com>
2022-12-10 22:00:44 -0500

    Update this script to replace the value 12 with 89:

---
+++ javascript-warm_up/12-object.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/*
+ * Write a script that prints two arguments passed to it, in the following format: “ is ”
+*/
+
+const myObject = {
+  type: 'object',
+  value: 12
+};
+console.log(myObject);
+myObject.value = 89;
+console.log(myObject);
