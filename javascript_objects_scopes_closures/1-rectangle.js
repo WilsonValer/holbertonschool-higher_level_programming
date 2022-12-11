a9c2ddcddd47a4cb34e2367355b09b74b057b10d
WilsonValer <nederig21@gmail.com>
2022-12-11 14:19:49 -0500

    Write a class Rectangle that defines a rectangle:

---
+++ javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+/*
+Write a script that prints 3 lines:”:
+*/
+
+class Rectangle {
+  constructor (h, w) {
+    this.height = h;
+    this.width = w;
+  }
+}
+module.exports = Rectangle;
