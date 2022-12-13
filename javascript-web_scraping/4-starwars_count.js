4716ba4f308f1f6d76279bc63ca1031db0aa6ecc
WilsonValer <nederig21@gmail.com>
2022-12-13 13:37:11 -0500

    was corrected the code

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -9,10 +9,12 @@ request(url, (error, response, body) => {
   if (error) console.log(error);
   const json = JSON.parse(body);
   let count = 0;
-  const searchedChar = 'https://swapi-api.hbtn.io/api/people/18/';
+  const search = 'https://swapi-api.hbtn.io/api/people/18/';
   for (let value = 0; value < json.results.length; value++) {
-    if (json.results[value].characters.includes(searchedChar)) {
-      count += 1;
+    for (let i = 0; i < json.results[value].characters.length; i++) {
+      if (json.results[value].characters[i].includes(search)) {
+        count += 1;
+      }
     }
   }
   console.log(count);
