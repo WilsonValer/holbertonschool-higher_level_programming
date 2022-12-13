4b3dd8f912d1352cdf42978aeaa31877dc1139e5
WilsonValer <nederig21@gmail.com>
2022-12-13 14:04:04 -0500

    was rewrite the code again

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,21 +1,20 @@
 #!/usr/bin/node
 /*
- *Write a script that reads and prints the content of a file.
+ * prints the number of movies where the character "Wedge Antilles" is present
  */
-const request = require('request');
-const url = process.argv[2];
 
-request(url, (error, response, body) => {
+const apiUrl = process.argv[2];
+const charToSearch = 18;
+const request = require('request');
+
+request.get(apiUrl, (error, response, body) => {
   if (error) console.log(error);
-  const json = JSON.parse(body);
-  let count = 0;
-  const search = 'https://swapi-api.hbtn.io/api/people/18/';
-  for (let value = 0; value < json.results.length; value++) {
-    for (let i = 0; i < json.results[value].characters.length; i++) {
-      if (json.results[value].characters[i].includes(search)) {
-        count += 1;
-      }
-    }
-  }
-  console.log(count);
+  const data = JSON.parse(body);
+  let films = data.results;
+  films = films.filter(
+    film => film.characters.find(
+      character => character.match(charToSearch)
+    )
+  );
+  console.log(films.length);
 });
